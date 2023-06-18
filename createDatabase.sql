1d30442dfc644e9e2069eb7f2f1c507936feea46
Miguel <69534716+miguelcimolin@users.noreply.github.com>
2023-06-18 01:34:24 -0300

    Alteração do código de criação do banco de dados e inserção do dicionário de dados.

--- createDatabase.sql
+++ createDatabase.sql
@@ -40,14 +40,6 @@ CREATE TABLE [Conta]
 GO
 
 
-CREATE NONCLUSTERED INDEX [fk_Cliente__Conta] ON [Conta] 
- (
-  [id_cliente] ASC
- )
-
-GO
-
-
 -- ************************************** [Cambio]
 CREATE TABLE [Cambio]
 (
@@ -66,67 +58,11 @@ CREATE TABLE [Cambio]
 GO
 
 
-CREATE NONCLUSTERED INDEX [fk_Conta__Cambio] ON [Cambio] 
- (
-  [id_conta] ASC
- )
-
-GO
-
-
--- ************************************** [Transacao]
-CREATE TABLE [Transacao]
-(
- [id_trans] int NOT NULL ,
- [id_conta] int NOT NULL ,
- [data]     datetime NOT NULL ,
- [tp_trans] varchar(64) NOT NULL ,
- [valor]    numeric(14,2) NOT NULL ,
-
-
- CONSTRAINT [pk_Transacao] PRIMARY KEY CLUSTERED ([id_trans] ASC),
- CONSTRAINT [fk_Conta__Transacao] FOREIGN KEY ([id_conta])  REFERENCES [Conta]([id_conta])
-);
-GO
-
-
-CREATE NONCLUSTERED INDEX [fk_Conta__Transacao] ON [Transacao] 
- (
-  [id_conta] ASC
- )
-
-GO
-
-
--- ************************************** [Emprestimo]
-CREATE TABLE [Emprestimo]
-(
- [id_emprest]       int NOT NULL ,
- [id_conta]         int NOT NULL ,
- [valor]            numeric(14,2) NOT NULL ,
- [taxa_juros]       tinyint NOT NULL ,
- [data_contratacao] date NOT NULL ,
- [data_venc]        date NOT NULL ,
-
-
- CONSTRAINT [pk_Emprestimo] PRIMARY KEY CLUSTERED ([id_emprest] ASC),
- CONSTRAINT [fk_Conta__Emprestimo] FOREIGN KEY ([id_conta])  REFERENCES [Conta]([id_conta])
-);
-GO
-
-
-CREATE NONCLUSTERED INDEX [fk_Conta__Emprestimo] ON [Emprestimo] 
- (
-  [id_conta] ASC
- )
-
-GO
-
-
 -- ************************************** [Investimento]
 CREATE TABLE [Investimento]
 (
  [id_invest]       int NOT NULL ,
+ [ativo]           bit NOT NULL ,
  [id_conta]        int NOT NULL ,
  [tp_investimento] varchar(64) NOT NULL ,
  [vlr_investido]   numeric(14,2) NOT NULL ,
@@ -140,11 +76,21 @@ CREATE TABLE [Investimento]
 GO
 
 
-CREATE NONCLUSTERED INDEX [fk_Conta__Investimento] ON [Investimento] 
- (
-  [id_conta] ASC
- )
+-- ************************************** [Emprestimo]
+CREATE TABLE [Emprestimo]
+(
+ [id_emprest]       int NOT NULL ,
+ [id_conta]         int NOT NULL ,
+ [valor_emprest]    numeric(14,2) NOT NULL ,
+ [taxa_juros]       tinyint NOT NULL ,
+ [data_contratacao] date NOT NULL ,
+ [data_venc]        date NOT NULL ,
+ [saldo_devedor]    numeric(14,2) NOT NULL ,
 
+
+ CONSTRAINT [pk_Emprestimo] PRIMARY KEY CLUSTERED ([id_emprest] ASC),
+ CONSTRAINT [fk_Conta__Emprestimo] FOREIGN KEY ([id_conta])  REFERENCES [Conta]([id_conta])
+);
 GO
 
 
@@ -152,6 +98,7 @@ GO
 CREATE TABLE [Cartao]
 (
  [id_cartao]   int NOT NULL ,
+ [ativo]       bit NOT NULL ,
  [id_conta]    int NOT NULL ,
  [numero]      varchar(16) NOT NULL ,
  [titular]     varchar(100) NOT NULL ,
@@ -166,11 +113,22 @@ CREATE TABLE [Cartao]
 GO
 
 
-CREATE NONCLUSTERED INDEX [fk_Conta__Cartao] ON [Cartao] 
- (
-  [id_conta] ASC
- )
+-- ************************************** [Transacao]
+CREATE TABLE [Transacao]
+(
+ [id_trans]   int NOT NULL ,
+ [id_conta]   int NOT NULL ,
+ [id_cliente] int NOT NULL ,
+ [data]       datetime NOT NULL ,
+ [tp_trans]   varchar(64) NOT NULL ,
+ [status]     varchar(40) NOT NULL ,
+ [valor]      numeric(14,2) NOT NULL ,
 
+
+ CONSTRAINT [pk_Transacao] PRIMARY KEY CLUSTERED ([id_trans] ASC),
+ CONSTRAINT [fk_Cliente__Transacao] FOREIGN KEY ([id_cliente])  REFERENCES [Cliente]([id_cliente]),
+ CONSTRAINT [fk_Conta__Transacao] FOREIGN KEY ([id_conta])  REFERENCES [Conta]([id_conta])
+);
 GO
 
 
@@ -178,71 +136,15 @@ GO
 CREATE TABLE [Fatura]
 (
  [id_fatura] int NOT NULL ,
+ [ativo]     bit NOT NULL ,
+ [id_trans]  int NOT NULL ,
  [id_conta]  int NOT NULL ,
- [id_cartao] int NOT NULL ,
- [valor]     numeric(14,2) NOT NULL ,
+ [valor]     numeric(14,2) NULL ,
  [dt_venc]   date NOT NULL ,
 
 
  CONSTRAINT [pk_Fatura] PRIMARY KEY CLUSTERED ([id_fatura] ASC),
- CONSTRAINT [fk_Cartao__Fatura] FOREIGN KEY ([id_cartao])  REFERENCES [Cartao]([id_cartao]),
- CONSTRAINT [fk_Conta__Fatura] FOREIGN KEY ([id_conta])  REFERENCES [Conta]([id_conta])
+ CONSTRAINT [fk_Conta__Fatura] FOREIGN KEY ([id_conta])  REFERENCES [Conta]([id_conta]),
+ CONSTRAINT [fk_Transacao__Fatura] FOREIGN KEY ([id_trans])  REFERENCES [Transacao]([id_trans])
 );
-GO
-
-
-CREATE NONCLUSTERED INDEX [fk_Cartao__Fatura] ON [Fatura] 
- (
-  [id_cartao] ASC
- )
-
-GO
-
-CREATE NONCLUSTERED INDEX [fk_Conta__Fatura] ON [Fatura] 
- (
-  [id_conta] ASC
- )
-
-GO
-
-
--- ************************************** [Pagamento]
-CREATE TABLE [Pagamento]
-(
- [id_pagamento]  int NOT NULL ,
- [id_conta]      int NOT NULL ,
- [id_cliente]    int NOT NULL ,
- [id_trans]      int NOT NULL ,
- [valor]         numeric(14,2) NOT NULL ,
- [dt_vencimento] date NOT NULL ,
- [status]        varchar(35) NOT NULL ,
-
-
- CONSTRAINT [pk_Pagamento] PRIMARY KEY CLUSTERED ([id_pagamento] ASC),
- CONSTRAINT [fk_Cliente__Pagamento] FOREIGN KEY ([id_cliente])  REFERENCES [Cliente]([id_cliente]),
- CONSTRAINT [fk_Conta__Pagamento] FOREIGN KEY ([id_conta])  REFERENCES [Conta]([id_conta]),
- CONSTRAINT [fk_Transacao__Pagamento] FOREIGN KEY ([id_trans])  REFERENCES [Transacao]([id_trans])
-);
-GO
-
-
-CREATE NONCLUSTERED INDEX [fk_Cliente__Pagamento] ON [Pagamento] 
- (
-  [id_cliente] ASC
- )
-
-GO
-
-CREATE NONCLUSTERED INDEX [fk_Conta__Pagamento] ON [Pagamento] 
- (
-  [id_conta] ASC
- )
-
-GO
-
-CREATE NONCLUSTERED INDEX [fk_Transacao__Pagamento] ON [Pagamento] 
- (
-  [id_trans] ASC
- )
-
 GO
