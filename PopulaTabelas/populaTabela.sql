2274ca1dc07786a717c01050f813af74fa3e6303
Miguel <69534716+miguelcimolin@users.noreply.github.com>
2023-06-23 14:53:36 -0300

    Correcao de nulls e novas colunas.

--- PopulaTabelas/populaTabela.sql
+++ PopulaTabelas/populaTabela.sql
@@ -3,7 +3,7 @@ set dateformat ymd  -- server para definir o formato do datetime em YY/MM/DD (en
 -- ************************************** [INSERTS Cliente]
 
 INSERT INTO Cliente (ativo, nome, cpf, data_nasc, phone, endereco, email, senha, dt_criacao)
-VALUES (1, 'Paulo Mendes', '11122233344', '2000-12-12', '912341234', 'Criciuma', 'paulomendes@gmail.com', '112233', '2022-04-22 11:30:00')
+VALUES (1, 'Paulo Mendes', '11122233344', '1980-12-12', '912341234', 'Criciuma', 'paulomendes@gmail.com', '112233', '2022-04-22 11:30:00')
 INSERT INTO Cliente (ativo, nome, cpf, data_nasc, phone, endereco, email, senha, dt_criacao)
 VALUES (1, 'Paulo Gomes', '11111111122', '1990-01-15', '943214321', 'Criciuma', 'paulogomes@gmail.com', '123123', '2021-02-27 08:30:00')
 INSERT INTO Cliente (ativo, nome, cpf, data_nasc, phone, endereco, email, senha, dt_criacao)
@@ -11,23 +11,23 @@ VALUES (1, 'Joao de Souza', '99999988877', '1980-05-18', '944445555', 'Florianop
 INSERT INTO Cliente (ativo, nome, cpf, data_nasc, phone, endereco, email, senha, dt_criacao)
 VALUES (1, 'Kelvin Cleo', '66677755544', '1993-04-01', '991235432', 'Gaspar', 'kelvincleo21@gmail.com', '998871', '2019-03-29 16:30:00')
 INSERT INTO Cliente (ativo, nome, cpf, data_nasc, phone, endereco, email, senha, dt_criacao)
-VALUES (1, 'Julio Fiodor', '17177766688', '1998-08-01', '999998888', 'Florianopolis', 'juliofiodor2@gmail.com', '556644', '2017-02-01 13:30:00')
+VALUES (1, 'Julio Fiodor', '17177766688', '1977-08-01', '999998888', 'Florianopolis', 'juliofiodor2@gmail.com', '556644', '2017-02-01 13:30:00')
 INSERT INTO Cliente (ativo, nome, cpf, data_nasc, phone, endereco, email, senha, dt_criacao)
-VALUES (0, 'Gustavo Nava', '22211133344', '2001-11-26', '993218765', 'Florianopolis', 'gustavonava998@gmail.com', '876876', '2023-01-01 16:50:00')
+VALUES (0, 'Gustavo Nava', '22211133344', '1981-11-26', '993218765', 'Florianopolis', 'gustavonava998@gmail.com', '876876', '2023-01-01 16:50:00')
 INSERT INTO Cliente (ativo, nome, cpf, data_nasc, phone, endereco, email, senha, dt_criacao)
 VALUES (1, 'Danilo Guindaste', '66655543422', '1999-12-17', '940028922', 'Criciuma', 'daniloguindastez@gmail.com', '554281', '2015-02-05 09:30:00')
 INSERT INTO Cliente (ativo, nome, cpf, data_nasc, phone, endereco, email, senha, dt_criacao)
-VALUES (1, 'Flavia Scussel', '88776655443', '1997-10-07', '911118888', 'Sideropolis', 'flaviascussel982@gmail.com', '123888', '2019-12-18 11:30:00')
+VALUES (1, 'Flavia Scussel', '88776655443', '1982-10-07', '911118888', 'Sideropolis', 'flaviascussel982@gmail.com', '123888', '2019-12-18 11:30:00')
 INSERT INTO Cliente (ativo, nome, cpf, data_nasc, phone, endereco, email, senha, dt_criacao)
 VALUES (0, 'Valeria Alavanca', '77711133344', '1989-08-21', '988877799', 'Sideropolis', 'valeriaalavanca@gmail.com', '332255', '2020-01-25 10:42:00')
 INSERT INTO Cliente (ativo, nome, cpf, data_nasc, phone, endereco, email, senha, dt_criacao)
-VALUES (0, 'Lucia Mattos', '77754522871', '1992-05-05', '917284928', 'Florianopolis', 'luciamattosxd@gmail.com', '102876', '2014-03-27 14:15:00')
+VALUES (0, 'Lucia Mattos', '77754522871', '1976-05-05', '917284928', 'Florianopolis', 'luciamattosxd@gmail.com', '102876', '2014-03-27 14:15:00')
 INSERT INTO Cliente (ativo, nome, cpf, data_nasc, phone, endereco, email, senha, dt_criacao)
 VALUES (1, 'Giovanna Lambreta', '33322288865', '2000-10-28', '999955555', 'Treviso', 'giovannalambreta322@gmail.com', '444555', '2014-06-18 10:55:00')
 INSERT INTO Cliente (ativo, nome, cpf, data_nasc, phone, endereco, email, senha, dt_criacao)
 VALUES (1, 'Ana Julia da Silva', '98172312344', '1995-06-16', '922448877', 'Criciuma', 'anajsilva1@gmail.com', '776612', '2015-01-01 17:00:00')
 INSERT INTO Cliente (ativo, nome, cpf, data_nasc, phone, endereco, email, senha, dt_criacao)
-VALUES (1, 'Eliana Demencio', '22211111144', '1997-01-25', '987658765', 'Laguna', 'elianademencio98@gmail.com', '325466', '2016-06-16 16:16:00')
+VALUES (1, 'Eliana Demencio', '22211111144', '1980-01-25', '987658765', 'Laguna', 'elianademencio98@gmail.com', '325466', '2016-06-16 16:16:00')
 INSERT INTO Cliente (ativo, nome, cpf, data_nasc, phone, endereco, email, senha, dt_criacao)
 VALUES (0, 'Chloe Regard', '98798798711', '2001-12-25', '999886251', 'Laguna', 'chloeregard123@gmail.com', '887123', '2017-07-21 14:10:00')
 INSERT INTO Cliente (ativo, nome, cpf, data_nasc, phone, endereco, email, senha, dt_criacao)
@@ -91,26 +91,26 @@ VALUES (1, 12, 'renda_fixa', 800000, 465000, '2017-12-29')
 
 -- ************************************** [INSERTS Emprestimo]
 
-INSERT INTO Emprestimo (id_conta, valor_emprest, taxa_juros, data_contratacao, data_venc, saldo_devedor)
-VALUES (5, 5000, 1.15, '2018-07-27', '2018-10-27', 5750)
-INSERT INTO Emprestimo (id_conta, valor_emprest, taxa_juros, data_contratacao, data_venc, saldo_devedor)
-VALUES (4, 1500, 1.15, '2019-05-07', '2019-08-07', 1725)
-INSERT INTO Emprestimo (id_conta, valor_emprest, taxa_juros, data_contratacao, data_venc, saldo_devedor)
-VALUES (11, 4000, 1.15, '2015-03-21', '2015-06-21', 4600)
-INSERT INTO Emprestimo (id_conta, valor_emprest, taxa_juros, data_contratacao, data_venc, saldo_devedor)
-VALUES (11, 4500, 1.15, '2016-02-10', '2016-05-10', 5175)
-INSERT INTO Emprestimo (id_conta, valor_emprest, taxa_juros, data_contratacao, data_venc, saldo_devedor)
-VALUES (11, 5000, 1.15, '2020-11-05', '2021-02-05', 5750)
-INSERT INTO Emprestimo (id_conta, valor_emprest, taxa_juros, data_contratacao, data_venc, saldo_devedor)
-VALUES (15, 6500, 1.15, '2016-01-20', '2018-04-20', 7475)
-INSERT INTO Emprestimo (id_conta, valor_emprest, taxa_juros, data_contratacao, data_venc, saldo_devedor)
-VALUES (15, 7000, 1.15, '2018-06-01', '2018-09-01', 8050)
-INSERT INTO Emprestimo (id_conta, valor_emprest, taxa_juros, data_contratacao, data_venc, saldo_devedor)
-VALUES (8, 15000, 1.25, '2021-04-04', '2022-04-04', 187500)
-INSERT INTO Emprestimo (id_conta, valor_emprest, taxa_juros, data_contratacao, data_venc, saldo_devedor)
-VALUES (2, 10000, 1.15, '2021-11-11', '2022-02-11', 11500)
-INSERT INTO Emprestimo (id_conta, valor_emprest, taxa_juros, data_contratacao, data_venc, saldo_devedor)
-VALUES (7, 17000, 1.25, '2017-06-25', '2018-06-25', 21250)
+INSERT INTO Emprestimo (ativo, id_conta, valor_emprest, taxa_juros, data_contratacao, data_venc, saldo_devedor)
+VALUES (1, 5, 5000, 1.15, '2023-05-27', '2023-08-27', 5750)
+INSERT INTO Emprestimo (ativo, id_conta, valor_emprest, taxa_juros, data_contratacao, data_venc, saldo_devedor)
+VALUES (0, 4, 1500, 1.15, '2019-05-07', '2019-08-07', 1725)
+INSERT INTO Emprestimo (ativo, id_conta, valor_emprest, taxa_juros, data_contratacao, data_venc, saldo_devedor)
+VALUES (0, 10, 4000, 1.15, '2015-03-21', '2015-06-21', 4600)
+INSERT INTO Emprestimo (ativo, id_conta, valor_emprest, taxa_juros, data_contratacao, data_venc, saldo_devedor)
+VALUES (0, 11, 4500, 1.15, '2016-02-10', '2016-05-10', 5175)
+INSERT INTO Emprestimo (ativo, id_conta, valor_emprest, taxa_juros, data_contratacao, data_venc, saldo_devedor)
+VALUES (1, 11, 5000, 1.15, '2023-04-05', '2023-10-05', 5750)
+INSERT INTO Emprestimo (ativo, id_conta, valor_emprest, taxa_juros, data_contratacao, data_venc, saldo_devedor)
+VALUES (0, 15, 3500, 1.15, '2016-01-20', '2018-04-20', 4025)
+INSERT INTO Emprestimo (ativo, id_conta, valor_emprest, taxa_juros, data_contratacao, data_venc, saldo_devedor)
+VALUES (0, 15, 3900, 1.15, '2018-06-01', '2018-09-01', 4485)
+INSERT INTO Emprestimo (ativo, id_conta, valor_emprest, taxa_juros, data_contratacao, data_venc, saldo_devedor)
+VALUES (1, 8, 15000, 1.25, '2023-04-04', '2024-04-04', 18750)
+INSERT INTO Emprestimo (ativo, id_conta, valor_emprest, taxa_juros, data_contratacao, data_venc, saldo_devedor)
+VALUES (0, 2, 10000, 1.15, '2021-11-11', '2022-02-11', 11500)
+INSERT INTO Emprestimo (ativo, id_conta, valor_emprest, taxa_juros, data_contratacao, data_venc, saldo_devedor)
+VALUES (0, 7, 17000, 1.25, '2017-06-25', '2018-06-25', 21250)
 
 -- ************************************** [INSERTS Cartao]
 
@@ -136,7 +136,37 @@ INSERT INTO Cartao (ativo, id_conta, numero, titular, dt_validade, code_sec)
 VALUES (1, 15, '4444222266661111', 'Wellington Trevoso', '2034-01-11', '8141')
 
 -- ************************************** [INSERTS Transacao]
-
+  
+INSERT INTO Transacao (id_conta, id_cliente, data, tp_trans, status, valor)
+VALUES(1, 1, '2020-01-01', 'Depósito', 'Aprovado', 500);
+INSERT INTO Transacao (id_conta, id_cliente, data, tp_trans, status, valor)
+VALUES(2, 2, '2020-01-02', 'Depósito', 'Aprovado', 3000);
+INSERT INTO Transacao (id_conta, id_cliente, data, tp_trans, status, valor)
+VALUES(3, 3, '2020-01-03', 'Depósito', 'Aprovado', 1100);
+INSERT INTO Transacao (id_conta, id_cliente, data, tp_trans, status, valor)
+VALUES(4, 4, '2020-01-04', 'Depósito', 'Aprovado', 100);
+INSERT INTO Transacao (id_conta, id_cliente, data, tp_trans, status, valor)
+VALUES(5, 5, '2020-01-05', 'Depósito', 'Aprovado', 9000);
+INSERT INTO Transacao (id_conta, id_cliente, data, tp_trans, status, valor)
+VALUES(6, 6, '2020-01-06', 'Depósito', 'Aprovado', 89000);
+INSERT INTO Transacao (id_conta, id_cliente, data, tp_trans, status, valor)
+VALUES(7, 7, '2020-01-07', 'Depósito', 'Aprovado', 300);
+INSERT INTO Transacao (id_conta, id_cliente, data, tp_trans, status, valor)
+VALUES(8, 8, '2020-01-08', 'Depósito', 'Aprovado', 150000);
+INSERT INTO Transacao (id_conta, id_cliente, data, tp_trans, status, valor)
+VALUES(9, 9, '2020-01-09', 'Depósito', 'Aprovado', 15000);
+INSERT INTO Transacao (id_conta, id_cliente, data, tp_trans, status, valor)
+VALUES(10, 10, '2020-01-10', 'Depósito', 'Aprovado', 58000);
+INSERT INTO Transacao (id_conta, id_cliente, data, tp_trans, status, valor)
+VALUES(11, 11, '2020-01-11', 'Depósito', 'Aprovado', 500);
+INSERT INTO Transacao (id_conta, id_cliente, data, tp_trans, status, valor)
+VALUES(12, 12, '2020-01-12', 'Depósito', 'Aprovado', 200000);
+INSERT INTO Transacao (id_conta, id_cliente, data, tp_trans, status, valor)
+VALUES(13, 13, '2020-01-13', 'Depósito', 'Aprovado', 45000);
+INSERT INTO Transacao (id_conta, id_cliente, data, tp_trans, status, valor)
+VALUES(14, 14, '2020-01-14', 'Depósito', 'Aprovado', 500);
+INSERT INTO Transacao (id_conta, id_cliente, data, tp_trans, status, valor)
+VALUES(15, 15, '2020-01-20', 'Depósito', 'Aprovado', 500);
 INSERT INTO Transacao (id_conta, id_cliente, data, tp_trans, status, valor)
 VALUES(1, 11, '2022-10-10', 'PIX', 'Aprovado', 1000);
 INSERT INTO Transacao (id_conta, id_cliente, data, tp_trans, status, valor)
@@ -146,7 +176,7 @@ VALUES(13, 4, '2022-10-12', 'PIX', 'Aprovado', 1000);
 INSERT INTO Transacao (id_conta, id_cliente, data, tp_trans, status, valor)
 VALUES(14, 11, '2022-10-13', 'PIX', 'Aprovado', 400);
 INSERT INTO Transacao (id_conta, id_cliente, data, tp_trans, status, valor)
-VALUES(1, 9, '2022-10-14', 'Debito', 'Aprovado', 50);
+VALUES(1, 11, '2022-10-14', 'Debito', 'Aprovado', 50);
 INSERT INTO Transacao (id_conta, id_cliente, data, tp_trans, status, valor)
 VALUES(7, 7, '2022-11-15', 'Deposito', 'Negado', 3000);
 INSERT INTO Transacao (id_conta, id_cliente, data, tp_trans, status, valor)
@@ -185,22 +215,22 @@ VALUES(15, 10, '2021-01-07', 'Credito', 'Negado', 230);
 -- ************************************** [INSERTS Fatura]
 
 INSERT INTO Fatura (ativo, id_trans, id_conta, valor, valor_emprest, dt_venc)
-VALUES (0, 8, 6, 150, null, '2022-04-21')
+VALUES (0, 7, 6, 150, 0, '2022-04-21')
 INSERT INTO Fatura (ativo, id_trans, id_conta, valor, valor_emprest, dt_venc)
-VALUES (1, 9, 1, 4000, null, '2022-04-22')
+VALUES (1, 8, 1, 4000, 0, '2022-04-22')
 INSERT INTO Fatura (ativo, id_trans, id_conta, valor, valor_emprest, dt_venc)
-VALUES (0, 16, 2, 1500, null, '2022-05-21')
+VALUES (0, 16, 2, 1500, 0, '2022-05-21')
 INSERT INTO Fatura (ativo, id_trans, id_conta, valor, valor_emprest, dt_venc)
-VALUES (1, 17, 6, 800, null, '2022-06-24')
+VALUES (1, 17, 6, 800, 0, '2022-06-24')
 INSERT INTO Fatura (ativo, id_trans, id_conta, valor, valor_emprest, dt_venc)
-VALUES (1, 18, 10, 750, null, '2022-06-24')
+VALUES (1, 18, 10, 750, 0, '2022-06-24')
 INSERT INTO Fatura (ativo, id_trans, id_conta, valor, valor_emprest, dt_venc)
-VALUES (1, 19, 15, 500, null, '2022-07-20')
+VALUES (1, 19, 15, 500, 0, '2022-07-20')
 INSERT INTO Fatura (ativo, id_trans, id_conta, valor, valor_emprest, dt_venc)
-VALUES (1, 20, 14, 3500, null, '2022-08-23')
+VALUES (1, 20, 14, 3500, 0, '2022-08-23')
 INSERT INTO Fatura (ativo, id_trans, id_conta, valor, valor_emprest, dt_venc)
-VALUES (1, 21, 6, 230, null, '2022-12-15')
+VALUES (1, 21, 6, 230, 0, '2022-12-15')
 INSERT INTO Fatura (ativo, id_trans, id_conta, valor, valor_emprest, dt_venc)
-VALUES (0, 3, 6, 1000, null, '2021-11-24')
+VALUES (0, 3, 6, 1000, 0, '2021-11-24')
 INSERT INTO Fatura (ativo, id_trans, id_conta, valor, valor_emprest, dt_venc)
-VALUES (0, 4, 6, 150, null, '2021-06-15')
+VALUES (0, 4, 6, 150, 0, '2021-06-15')
